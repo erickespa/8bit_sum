00f05a99b1d58c2edef4c6949a2d18da4d2ad762
ERICK NEFTALÍ ESPAÑA PERALTA <123218316+erickespa@users.noreply.github.com>
2025-06-03 11:53:51 -0600

    Update tt_um_erickespa.v

--- src/tt_um_erickespa.v
+++ src/tt_um_erickespa.v
@@ -1,9 +1,9 @@
 `default_nettype none
 
 module tt_um_erickespa (
-    input  wire [7:0] ui_in,    // Dedicated inputs (A)
-    output wire [7:0] uo_out,   // Dedicated outputs (sum[7:0])
-    input  wire [7:0] uio_in,   // IOs: Input path (B)
+    input  wire [7:0] ui_in,    // inputs A
+    output wire [7:0] uo_out,   // outputs ULA
+    input  wire [7:0] uio_in,   // IOs: Input B
     output wire [7:0] uio_out,  // IOs: Output path
     output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
     input  wire       ena,      // always 1 when the design is powered, so you can ignore it
@@ -12,16 +12,50 @@ module tt_um_erickespa (
 );
 
   // Internal wires
-  wire [8:0] sum;  // 9 bits to hold overflow
+  wire [8:0] sum;  
+    wire [7:0] rest;
+    wire [7:0] opand;
+    wire [7:0] opor;
+    wire [7:0] izq;
+    wire [7:0] dere;
+    wire [2:0] ula = uio_in[7:5];  //utilice los ultimos 3 bits de B como entradas a la ULA por que ya no hay mas entradas.
+    reg [7:0] result;
 
-  // Perform 8-bit addition
-  assign sum = ui_in + uio_in;
+  //suma
+  assign sum = ui_in + uio_in[4:0];
+  //resta
+  assign rest = ui_in - uio_in[4:0];
+  //and
+  assign opand = ui_in & uio_in[4:0];
+  //or
+  assign opor = ui_in | uio_in[4:0];
+  //shift izq
+    assign izq = {ui_in[6:0], 1'b0};
+    //shift derecha
+    assign dere = {1'b0, ui_in[7:1]};
+    
 
-  // Assign the lower 8 bits of the result to output
-  assign uo_out = sum[7:0];
+      always @(*) begin
+    case (ula)
+      3'b000: result = sum[7:0];
+      3'b001: result = rest;
+      3'b010: result = opand;
+      3'b011: result = opor;
+      3'b100: result = izq;
+      3'b101: result = dere;
+         default: result = 8'b00000000;
+    endcase
+  end
 
-  // Optionally: use the 9th bit as overflow indication on a GPIO output
-  assign uio_out = {7'b0, sum[8]}; 
+
+        //asignar flags
+wire carry = sum[8] & ~ula[1];
+wire zero = ~|result;
+wire negative = result[7];
+wire overflow = (~ula[1]) & (ui_in[7] ^ result[7]) & ~(ula[0] ^ ui_in[7] ^ uio_in[4]);
+  
+ assign uo_out = result;
+  assign uio_out = {4'b0000, carry, overflow, negative, zero};
   assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
